30343423b60ddaef9ad3b39d42b908d5b801a1e1
Evgenii <emeleshkov@mail.ru>
2021-06-05 14:39:20 +0300

    sorted method

--- Contents.swift
+++ Contents.swift
@@ -22,3 +22,22 @@ func isEqual (str1: String, str2: String) -> Bool {
 isEqual(str1: "Hello, world!", str2: "Hello, world!")  //true
 isEqual(str1: "Hello, world!", str2: "Hello, World!")  //false
 //____________________________________________________________
+
+
+//sorted method:
+func isEqualSorted (str1: String, str2: String) -> Bool {
+    return str1.sorted() == str2.sorted()
+}
+
+isEqualSorted(str1: "Hello!", str2: "Hello!")   //true
+isEqualSorted(str1: "loHel!", str2: "Hello!")   //true
+isEqualSorted(str1: "qwerty", str2: "qwertyU")  //false
+
+/*method sorted anotation:
+ 
+func sorted() -> [Character]
+
+You can sort any sequence of elements that conform to the Comparable protocol by calling this method. Elements are sorted in ascending order.
+ 
+ The sorting algorithm is not guaranteed to be stable. A stable sort preserves the relative order of elements that compare equal.
+ */
